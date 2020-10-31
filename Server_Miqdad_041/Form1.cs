d1c328340cafac093825a25f4cae1bc1c8a8dbc1
Muhammad Miqdad Ghifar <mmiqdad76@gmail.com>
2020-10-31 15:16:18 +0700

    Add project files.

---
+++ Server_Miqdad_041/Form1.cs
@@ -0,0 +1,67 @@
+﻿using ServerdanClient_MuhammadMiqdadGhifar_041.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Miqdad_041
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(iMatematika));
+                hostObj.Open();
+
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                button2.Enabled = false;
+                button1.Enabled = true;
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(iMatematika));
+                hostObj.Close();
+
+                button2.Enabled = false;
+                button1.Enabled = true;
+                Console.WriteLine("Server's OFF");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
---
+++ ServerdanClient_MuhammadMiqdadGhifar_041/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerdanClient_MuhammadMiqdadGhifar_041
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.iMatematika obj = new ServiceReference1.iMatematika();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int angka1 = Int32.Parse(textBox1.Text);
+            int angka2 = Int32.Parse(textBox2.Text);
+
+
+
+            label7.Text = obj.Tambah(angka1, angka2).ToString();
+            label8.Text = obj.Kurang(angka1, angka2).ToString();
+            label9.Text = obj.Kali(angka1, angka2).ToString();
+            label10.Text = obj.Bagi(angka1, angka2).ToString();
+
+
+
+        }
+    }
+}
